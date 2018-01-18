22d4bb6c08f00d65c3ceaccf21076d7843017862
alr1970 <andrew@laurencefield.com>
2018-01-18 11:50:06 +0000

    Added getopt for command line processing

---
+++ Project/PhotoDeduplicator/Project1/getopt.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern char    *optarg;
+int getopt(int nargc, char * const nargv[], const char * ostr);
--- Project/PhotoDeduplicator/Project1/main.cpp
+++ Project/PhotoDeduplicator/Project1/main.cpp
@@ -1,4 +1,14 @@
+#include "getopt.h"
+#include <string>
 int main(int argc, char *argv[])
 {
+	int c;
+	std::string sourceRoot;
+	while ((c = getopt(argc, argv, "s:")) != -1)
+		switch (c)
+		{
+		case 's':
+			sourceRoot = optarg;
+		}
 
 }
